7f715ecb4ad42663c33ad19c0734e172db4378fc
Sherliane <sherliane.ifrn@gmail.com>
2023-12-11 23:09:58 -0300

    algumas implementacoes

--- src/app/(authenticated)/listPlanning/page.tsx
+++ src/app/(authenticated)/listPlanning/page.tsx
@@ -1,10 +1,233 @@
+"use client";
+import { useApi } from "@/hooks/useApi";
+import React, { useState } from "react";
+import { any, date, z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import Button from "@/components/Button";
+import { Icon } from "@iconify/react/dist/iconify.js";
+
+const select = z.object({
+  id: z.number(),
+});
+
+const validate = z.object({
+  id: z.number(),
+  month: z.string(),
+  hasCategory: z.array(z.object({
+    category: z.any(),
+    valuePerCategory: z.number(),
+  })),
+
+});
+
+type createPlanningFormData = z.infer<typeof select>;
+type createOptionsFormData = z.infer<typeof validate>;
+
+
 export default function Planning() {
-    return (
-        <div className="container mx-auto px-4 flex min-h-screen flex-col bg-white">
-            <p className="text-xl mt-5">Planejamentos</p>
-            <p className="text-sm text-[#908B8B]">
-                Listagem de planejamentos mensais
-            </p>    
+  const [planning, setPlanning] = useState<createOptionsFormData[]>([])
+  const [data, setData] = useState<createOptionsFormData[]>([]);
+
+  const [options, setOptions] = useState<createOptionsFormData[]>([]);
+
+
+  function convert(data: any) {
+    if (data) {
+      const [ano, mes] = data.split("-");
+      const valor = parseInt(mes);
+      var month = [
+        "",
+        "Janeiro",
+        "Fevereiro",
+        "Março",
+        "Abril",
+        "Maio",
+        "Junho",
+        "Julho",
+        "Agosto",
+        "Setembro",
+        "Outubro",
+        "Novembro",
+        "Dezembro",
+      ];
+      return `${month[valor]} de ${ano}`;
+    } else {
+      return null;
+    }
+  }
+
+  const {
+    register,
+    handleSubmit,
+    watch,
+    control,
+    formState: { errors },
+  } = useForm<createPlanningFormData>({
+    mode: "all",
+    criteriaMode: "all",
+    resolver: zodResolver(select),
+  });
+
+  React.useEffect(() => {
+    (async () => {
+      await useApi("get", "/planning", planning)
+        .then((response) => {
+          return setPlanning(response), setOptions(response);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    })();
+  }, []);
+
+  async function onSubmit() {
+    await useApi("get", `/planning/${id}`)
+      .then((response) => {
+        return setData(response), setPlanning([]);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  async function clear() {
+    setPlanning(options)
+    setData([])
+  }
+
+  async function remove(i: number) {
+    await useApi("delete", `/planning/${i}`)
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  const id = watch("id");
+
+  return (
+    <div className="container mx-auto px-4 flex min-h-screen flex-col bg-white">
+      <p className="text-xl mt-5">Planejamentos</p>
+      <p className="text-sm text-[#908B8B]">
+        Listagem de planejamentos mensais
+      </p>
+      <p className="mt-8">Selecione o planejamento que deseja mostrar:</p>
+      <div className="grid grid-cols-8">
+        <select
+          {...register(`id`, {
+            required: true,
+            valueAsNumber: true,
+          })}
+          defaultValue={"DEFAULT"}
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-50 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 w-40"
+        >
+          <option value="DEFAULT" disabled>
+            selecione o mês
+          </option>
+          {options
+            ? options.map((item) => (
+                <option key={item.id} value={item.id}>
+                  {convert(item.month)}
+                </option>
+              ))
+            : null}
+        </select>
+        <Button
+          className="w-20 ml-12"
+          type={"submit"}
+          onClick={handleSubmit(onSubmit)}
+        >
+          Buscar
+        </Button>
+        <Button
+          className="w-20 ml-4"
+          type={'button'}
+          onClick={(clear)}
+        >
+          Limpar
+        </Button>
+      </div>
+      <div className="flex flex-col items-center">
+        <div>
+          <p className=" text-xl text-[#6174EE]">
+            <b>Análises e Despesas </b>
+          </p>
         </div>
-    );
+        <div>
+          <b className="text-xl text-[#1E90FF]">
+            {data
+              ? data.slice(0, 1).map((item) => convert(item.month))
+              : null}
+          </b>
+        </div>
+      </div>
+      {planning && data
+        ? planning.map((item, i) => (
+            <table className="mt-5" key={i}>
+              <thead>
+                <tr>
+                  <th>
+                    <button onClick={() => remove(item.id)}>
+                      <Icon
+                        icon="ph:trash"
+                        className="text-[#6174EE]"
+                        width="24"
+                        height="24"
+                      />
+                    </button>
+                  </th>
+                  <th className="text-xl align-center text-[#1E90FF]">
+                    {convert(item.month)}
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr className="bg-[#6174EE] text-white rounded-md">
+                  <td>Categoria</td>
+                  <td>Expectativa de gasto</td>
+                  <td>Despesa do mês</td>
+                  <td>Situação</td>
+                </tr>
+                {item.hasCategory
+                  ? item.hasCategory.map((item, index) => (
+                      <tr key={index}>
+                        <td> {item.category["name"]}</td>
+                        <td>R${item.valuePerCategory}</td>
+                      </tr>
+                    ))
+                  : null}
+              </tbody>
+            </table>
+          ))
+        : null}
+      {data ? (
+        data.map((item, i) => (
+            <table className="mt-5"  key={item.id}>
+              <tbody>
+                <tr className="bg-[#6174EE] text-white rounded-md">
+                  <td>Categoria</td>
+                  <td>Expectativa de gasto</td>
+                  <td>Despesa</td>
+                  <td>Situação</td>
+                </tr>
+
+                {item.hasCategory
+                  ? item.hasCategory.map((item, index) => (
+                      <tr key={index}>
+                        <td> {item.category["name"]}</td>
+                        <td>R${item.valuePerCategory}</td>
+                      </tr>
+                    ))
+                  : null}
+              </tbody>
+            </table>
+        ))
+      ) : (
+        <p>'Selecione o mês para análise'</p>
+      )}
+    </div>
+  );
 }
--- src/app/(authenticated)/registerPlanning/page.tsx
+++ src/app/(authenticated)/registerPlanning/page.tsx
@@ -1,59 +1,240 @@
+"use client";
+import React, { useEffect, useState } from "react";
 import Button from "../../../components/Button";
+import { useApi } from "@/hooks/useApi";
+import { Icon } from "@iconify/react";
+import { useForm, useFieldArray } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const hasCategorySchema = z.array(
+  z.object({
+    category: z.number().positive("Informe um valor válido"),
+    valuePerCategory: z.number().positive("Informe um valor válido"),
+  })
+);
+const planning = z
+  .object({
+    month: z.string().min(1, "Informar o mês do planejamento é obrigatório."),
+    value: z.number().min(1,"Informe um valor válido"),
+    hasCategory: hasCategorySchema,
+  })
+  .refine((fields) => fields.hasCategory.length > 0, {
+    path: ["hasCategory"],
+    message: "Informe pelo menos uma categoria",
+  });
+
+type createPlanningFormData = z.infer<typeof planning>;
 
 export default function Planning() {
-    return (
-        <div className="container mx-auto px-4 flex min-h-screen flex-col bg-white">
-            <p className="text-xl mt-5">Planejamentos</p>
-            <p className="text-sm text-[#908B8B]">
-                Gerencie seus planejamentos mensais
-            </p>
-            <div className="mt-12">
-                <p className="text-center text-xl text-[#6174EE]">
-                    <b>Registrar Planejamento</b>
-                </p>
-                <hr className="mt-3 mb-4"></hr>
-                <p>
-                    <b>Selecione o mês que deseja gerar o planejamento:</b>
-                </p>
-                <input className="w-40 bg-[#E9E9FF] h-8 rounded" id="date" type="month"/>
-                <p className="mt-5">
-                    <b>Selecione o valor que pretende gastar no mês:</b>
-                </p>
-                <input className="w-40 bg-[#E9E9FF] h-8 rounded" id="number" type="number" step="0.01" placeholder="R$ 0,00"/>
-                <div className="flex justify-end">
-                    <Button>+ Salvar</Button>
-                </div>
-                <hr className="mt-3 mb-4"></hr>
-                <div className="grid justify-items-center">
-                    <p className=" text-xl text-[#6174EE]">
-                        <b>Metas e Orçamentos</b>
-                    </p>
-                    <p className="text-sm text-[#908B8B]">
-                        Atribua um valor a cada categoria
-                    </p>
-                </div>
-                <table className="border-separate border-spacing-2 border-slate-400 table-fixed bg-[#E9E9FF]">
-                    <thead>
-                        <tr>
-                            <th>Categorias</th>
-                            <th>Valor</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td className="border-slate-300">Aluguel</td>
-                            <td className="border-slate-300 "><input className="w-40 bg-[#E9E9FF] h-8 rounded" id="number" type="number" step="0.01" placeholder="R$ 0,00"/></td>
-                        </tr>
-                        <tr>
-                            <td className="border-slate-300">Categoria</td>
-                            <td className="border-slate-300 "><input className="w-40 bg-[#E9E9FF] h-8 rounded" id="number" type="number" step="0.01" placeholder="R$ 0,00"/></td>
-                        </tr>
-                    </tbody>
-                </table>
-                <div className="flex justify-end">
-                    <Button>Finalizar</Button>
-                </div>
+  const {
+    register,
+    handleSubmit,
+    watch,
+    control,
+    formState: { errors },
+  } = useForm<createPlanningFormData>({
+    mode: "all",
+    criteriaMode: "all",
+    resolver: zodResolver(planning),
+  });
+
+  const [category, setCategorys] = useState([{ id: "", name: "" }]);
+
+  const [availableValue, setAvailable] = React.useState(0);
+  const [value, setValue] = React.useState(0);
+
+  const valuePerCategory = watch("hasCategory");
+
+  const { fields, append, remove } = useFieldArray({
+    control,
+    name: "hasCategory",
+  });
+
+  React.useEffect(() => {
+    (async () => {
+      await useApi("get", "/categorys", category)
+        .then((response) => {
+          return setCategorys(response.result);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    })();
+  }, []);
+
+  function diminuirValorDisponivel() {
+    valuePerCategory.map((item) => {
+      setAvailable(value - item.valuePerCategory);
+    });
+  }
+
+  function aumentarValorDisponivel() {
+    valuePerCategory.map((item) => {
+      setAvailable(value + item.valuePerCategory);
+    });
+  }
+
+  function handleClick() {
+    diminuirValorDisponivel(),
+    append({
+      category: 0,
+      valuePerCategory: 0,
+    });
+  }
+
+  function handleClick2(id: number) {
+    aumentarValorDisponivel(), remove(id);
+  }
+
+  const onSubmit = (data: any) => {
+    useApi("post", "/planning", data)
+      .then((response) => console.log(response))
+      .catch((error) => console.log(error));
+  };
+
+  return (
+    <div className="container mx-auto px-4 flex min-h-screen flex-col bg-white">
+      <p className="text-xl mt-5">Planejamentos</p>
+      <p className="text-sm text-[#908B8B]">
+        Gerencie seus planejamentos mensais
+      </p>
+      <form onClick={handleSubmit(onSubmit)}>
+        <div className="mt-12">
+          <p className="text-center text-xl text-[#6174EE]">
+            <b>Registrar Planejamento</b>
+          </p>
+          <hr className="mt-3 mb-4"></hr>
+          <div>
+            <p>Selecione o mês que deseja gerar o planejamento:</p>
+            <input
+              className="w-40 bg-[#E9E9FF] h-8 rounded"
+              id="date"
+              type="month"
+              {...register("month", { required: true })}
+            />
+            {/* {errors.value && <p>{errors.month?.message}</p>} */}
+
+            <p className="mt-8">Informe o valor que pretende gastar no mês:</p>
+          </div>
+          <div>
+            <input
+              className="w-40 bg-[#E9E9FF] h-8 rounded"
+              id="number"
+              type="number"
+              placeholder=""
+              {...register("value", { required: true, valueAsNumber: true })}
+              onChange={(event: any) => [setAvailable(event.target.value), setValue(event.target.value)]}
+            />
+            {/* {errors.value && <p>{errors.value.message}</p>} */}
+          </div>
+          <hr className="mt-3 mb-4"></hr>
+          <div>
+            <div className="grid justify-items-center">
+              <p className=" text-xl text-[#6174EE]">
+                <b>Metas e Orçamentos</b>
+              </p>
+              <p className="text-sm text-[#908B8B]">
+                Atribua um valor a cada categoria
+              </p>
             </div>
+            <div className="grid grid-cols-12 ml-3 mt-5">
+              <Icon
+                icon="tabler:pig-money"
+                className="text-[#6174EE]"
+                width="24"
+                height="24"
+              />
+                <p className="ml-25">${availableValue ? availableValue : 0.0}</p>
+                {/* <p>{...register("planning.value")}</p> */}
+                <Icon
+                  icon="nimbus:money"
+                  className="text-[#6174EE] ml-20"
+                  width="24"
+                  height="24"
+                />
+              <p className="ml-20">R${value ? value : 0.0}</p>
+            </div>
+            <div className="grid grid-cols-3">
+              <p className="text-sm text-[#908B8B]">
+                Valor restante disponível
+              </p>
+              <p className="text-sm text-[#908B8B]">
+                Valor total informado
+              </p>
+            </div>
+          </div>
+          {fields.map((field, index) => (
+            <div className="mt-5" key={field.id}>
+              <div className="grid grid-cols-3">
+                <p>Selecione a categoria:</p>
+                <p className="ml-2">Informe o valor previsto de gasto:</p>
+              </div>
+              <div className="grid grid-cols-3">
+                <select
+                  // defaultValue={"DEFAULT"}
+                  {...register(`hasCategory.${index}.category`, {
+                    required: true,
+                    valueAsNumber: true,
+                  })}
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-50 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                >
+                  <option value="DEFAULT" disabled>
+                    nenhuma categoria selecionada
+                  </option>
+                  {category.map((categoria) => (
+                    <option key={categoria.id} value={categoria.id}>
+                      {categoria.name}
+                    </option>
+                  ))}
+                </select>
+                {/* {errors.value && <p>{errors.hasCategory?.map((item) =>item?.category?.message)}</p>} */}
+
+                <input
+                  className="ml-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-50 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  type="number"
+                  step="0.01"
+                  placeholder="R$ 0,00"
+                  {...register(`hasCategory.${index}.valuePerCategory`, {
+                    required: true,
+                    valueAsNumber: true,
+                  })}
+                ></input>
+                {/* {errors.value && <p>{errors.hasCategory?.map((item) =>item?.valuePerCategory?.message)}</p>} */}
+
+                <Button
+                  type="button"
+                  className="w-10 ml-2 text-center rounded-full"
+                  iconName="gg:remove"
+                  onClick={() => remove(index)}
+                />
+              </div>
+            </div>
+          ))}
+          <Button
+            type="button"
+            className="w-10 ml-2 text-center rounded-full mt-5"
+            iconName="gg:add"
+            onClick={() =>
+              append({
+                category: 0,
+                valuePerCategory: 0,
+              })
+            }
+          />
+          <div className="flex justify-end">
+            <Button type="submit">Finalizar</Button>
+          </div>
         </div>
-    );
+      </form>
+    </div>
+  );
+}
+
+{
+  /* // const handlePhoneNumberChange = (e: { target: { value: any } }) => {
+//     const input = e.target.value;
+//     const formatted = formatPhoneNumber(input);
+//     setValue("phone", formatted);
+// }; */
 }
